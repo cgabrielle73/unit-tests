6905f24b154321062cf7b1bcea40c5e0eb7f4867
Clara <clara.gabrielle@youxgroup.com.br>
2023-08-01 18:49:15 -0300

    estudando sobre mocks e spies com jest

--- script2.test.js
+++ script2.test.js
@@ -21,3 +21,21 @@ it('calls getPeoplePromise function to get people', () => {
         expect(data.results.length).toBeGreaterThan(3)
     })
 })
+
+
+it('mock getPeoplePromise', () => {
+    const mockFetch = jest.fn().mockReturnValue(Promise.resolve({
+        json: () => Promise.resolve({
+            count: 82,
+            results: [1,2,3,4,5,6]
+        })
+    }))
+
+    expect.assertions(4);
+    return getPeoplePromise(mockFetch).then(data => {
+        expect(mockFetch.mock.calls.length).toBe(1);
+        expect(mockFetch).toBeCalledWith('https://swapi.dev/api/people');
+        expect(data.count).toEqual(82)
+        expect(data.results.length).toBeGreaterThan(3)
+    })
+})
